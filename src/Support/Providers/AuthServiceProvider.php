52ad11675e30909c164476ffc925e4b6d0a3f5b6
Laurence <laurencehr@gmail.com>
2023-08-15 14:57:37 -0500

    UserAuthProvider con hash

--- src/Support/Providers/AuthServiceProvider.php
+++ src/Support/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace Jaguar\Core\Security\Support\Providers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Contracts\Hashing\Hasher as HasherContract;
+use DxsRavel\Essentials\Auth\Md5Hasher;
 
 use Jaguar\Core\Security\Auth\UserAuthProvider;
 
@@ -17,8 +18,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Auth::provider('jaguar-auth-driver', function ($app, array $config) {
-            return new UserAuthProvider($app['hash'], $config['model'], $config['request']);
+        Auth::provider('jaguar-auth-driver', function ($app, array $config) {                    
+            $hash = isset($config['hash']) ? new $config['hash']() : $app['hash'];            
+            return new UserAuthProvider($hash, $config['model'], $config['request']);
         }); 
     }
 }
